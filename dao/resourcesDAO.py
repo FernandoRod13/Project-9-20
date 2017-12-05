6b99743be7478e39b35502f51397c7b7cb32735f
natipr <natalia.benitez1@upr.edu>
2017-12-05 00:32:32 -0400

    erge branch 'natalia' of https://github.com/FernandoRod13/Project-9-20 into natalia

--- dao/resourcesDAO.py
+++ dao/resourcesDAO.py
@@ -1,6 +1,6 @@
-from flask import jsonify
 import json
 
+from flask import jsonify
 
 
 class ResourceDAO:
@@ -87,3 +87,59 @@ class ResourceDAO:
         return sorted(temp, key=lambda k: k['name'])
 
     
+
+    def getCategoryRequestedWater(self):
+        return ' Water Category'
+    #
+    # ****************** DASHBOARD METHODS *********************
+    #
+
+    # DAILY STATISTICS
+    def getAllDailyResInNeed(self):
+        result = []
+        result.append("Daily resources in need")
+        return result
+    
+    def getAllDailyResAvailable(self):
+        result = []
+        result.append("Daily resources available")
+        return result
+    
+    def getAllDailyResBetween(self):
+        result = []
+        result.append("Daily resources between")
+        return result
+    
+    # TRENDING STATISTICS (7 DAY PERIOD)
+    def getAllTrendingResInNeed(self):
+        result = []
+        result.append("Trending resources in need")
+        return result
+    
+    def getAllTrendingResAvailable(self):
+        result = []
+        result.append("Trending resources available")
+        return result
+    
+    def getAllTrendingResBetween(self):
+        result = []
+        result.append("Trending resources between")
+        return result 
+    
+    # TRENDING STATICTICS (8 SENATES)
+    def getAllTrendingResInNeedBySenate(self, keywords):
+        result = []
+        result.append("Trending resources by" + keywords + " in need")
+        return result
+    
+    def getAllTrendingResAvailableBySenate(self, keywords):
+        result = []
+        result.append("Trending resources by" + keywords + " available")
+        return result
+    
+    def getAllTrendingResBetweenBySenate(self, keywords):
+        result = []
+        result.append("Trending resources by" + keywords + " in between")
+        return result
+ 
+   
