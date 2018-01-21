c23f3ddd3f5987dfbcf6233fb9c08c44c32e35cf
Natalia <natalia.benitez1@upr.edu>
2018-01-21 16:30:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -224,6 +224,13 @@ def getSupplierTransaction():
 def getResourceTransaction():
     if request.method == 'GET':
         return TransactionHandler().getTransactionByResource(request.args)
+    
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  return response
 
 if __name__ == '__main__':
     app.run()
