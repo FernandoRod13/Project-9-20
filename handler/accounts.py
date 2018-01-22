7d36104623f89e380f538b8b2a6b858b83f102df
natipr <natalia.benitez1@upr.edu>
2018-01-22 19:12:29 -0400

    Fixed a typo

--- handler/accounts.py
+++ handler/accounts.py
@@ -10,6 +10,15 @@ class AccountHandler:
     
     def __init__(self):
             pass
+
+    def hash_password(self,password):
+            # uuid is used to generate a random number
+        salt = uuid.uuid4().hex
+        return hashlib.sha256(salt.encode() + password.encode()).hexdigest() + ':' + salt
+    
+    def check_password(self, hashed_password, user_password):
+        password, salt = hashed_password.split(':')
+        return password == hashlib.sha256(salt.encode() + user_password.encode()).hexdigest()
     
     def build_admin(self,row):
         result = {}
@@ -22,7 +31,7 @@ class AccountHandler:
         return result
 
     def getAllAdmin(self):
-        dao = AdminDAO()
+        dao = AccountsDAO()
         res = dao.getAllAdmin()
         result_list = []
         
@@ -35,12 +44,12 @@ class AccountHandler:
             return jsonify(Administrator = result_list)
 
     def getAdminByID(self,id):
-        dao = SupplierDAO()
+        dao = AccountsDAO()
         row = dao.searchAdminByID(id)
         if not row:
             return jsonify(Error = "Admin Not Found"), 404
         else:
-            part = self.build_supplier(row)
+            part = self.build_admin(row)
             return jsonify(Supplier = part)
 
     def userLogin(self, form):
@@ -48,7 +57,7 @@ class AccountHandler:
         password = form['password']
         
         if password and email and (len(form)==2):
-            dao = AdminDAO()
+            dao = AccountsDAO()
             password = hash_password(password)
             result = dao.accountLogin(email,password)
             if (len(result)==0):
@@ -62,14 +71,7 @@ class AccountHandler:
 ###################################################################
 ###########  POST
 
-    def hash_password(self,password):
-        # uuid is used to generate a random number
-        salt = uuid.uuid4().hex
-        return hashlib.sha256(salt.encode() + password.encode()).hexdigest() + ':' + salt
-    
-    def check_password(self, hashed_password, user_password):
-        password, salt = hashed_password.split(':')
-        return password == hashlib.sha256(salt.encode() + user_password.encode()).hexdigest()
+   
 
     def insertAdmin(self, form):
         first_name = form['first_name']
@@ -86,7 +88,7 @@ class AccountHandler:
         #Hash password
         password = self.hash_password(password)
         if first_name and last_name and email and phone and address and city_id and latitude and longitud and photo_url and password :
-            dao = AdminDAO()
+            dao = AccountsDAO()
             id = dao.addSupplier(first_name , last_name , email , phone , address , city_id , latitude , longitud , photo_url , 'Administrator' , password,dt)
             result = self.getAdminByID(id)
             return jsonify(Admin=result), 201
@@ -115,31 +117,31 @@ class AccountHandler:
         #Hash password
         password = self.hash_password(password)
         if first_name and last_name and email and phone and address and city_id and latitude and longitud and photo_url and password :
-            dao = AdminDAO()
+            dao = AccountsDAO()
             tid = dao.updateAdmin(id,first_name , last_name , email , phone , address , city_id , latitude , longitud , photo_url , 'Administrator' , password,dt)
             result = self.getAdminByID(tid)
             return jsonify(Admin=result), 201
 
         elif email and id and (len(form)==2):
-            dao = AdminDAO()
+            dao = AccountsDAO()
             tid = dao.updateAdminEmail(id,email)
             result = self.getAdminByID(tid)
             return jsonify(Admin=result), 201
 
         elif phone and id and (len(form)==2):
-            dao = AdminDAO()
+            dao = AccountsDAO()
             tid = dao.updateAdminPhone(id,phone)
             result = self.getAdminByID(tid)
             return jsonify(Admin=result), 201
 
         elif first_name and id and (len(form)==2):
-            dao = AdminDAO()
+            dao = AccountsDAO()
             tid = dao.updateAdminFirst_name(id,first_name)
             result = self.getAdminByID(tid)
             return jsonify(Admin=result), 201
 
         elif last_name and id and (len(form)==2):
-            dao = AdminDAO()
+            dao = AccountsDAO()
             tid = dao.updateAdminLast_name(id,last_name)
             result = self.getAdminByID(tid)
             return jsonify(Admin=result), 201    
